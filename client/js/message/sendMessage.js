148549afd9103984674e1de9868384c3e85162ed
anran <13654067079@163.com>
2018-12-10 15:50:11 +0800

    移除无用代码

--- client/js/message/sendMessage.js
+++
@@ -1,7 +0,0 @@
-function sendMessageHandler(userName, message,chatSocket) {
-    var jsonObject = {
-        userName: userName,
-        message: message
-    };
-    chatSocket.json.send(jsonObject);
-}
